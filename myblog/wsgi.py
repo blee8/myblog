23545564b2553d70536a4f993385fc075222fe8f
blee8 <blee8634@gmail.com>
2020-08-26 18:05:24 +0900

    1

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -1,12 +1,16 @@
+"""
+WSGI config for mysite2 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-
-path = '/home/blee8634/myblog'  # PythonAnywhere 계정으로 바꾸세요.
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myblog.settings'
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+
+application = get_wsgi_application()
